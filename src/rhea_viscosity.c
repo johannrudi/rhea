58b50b531aba0fc53a4f9f5db5f650990ee96ae8
Johann Rudi <johann@ices.utexas.edu>
2016-10-15 15:19:41 -0500

    rhea_viscosity: Adapt functions from slabs example.

--- src/rhea_viscosity.c
+++ src/rhea_viscosity.c
@@ -5,66 +5,64 @@
 #include <rhea_base.h>
 
 /**
- * Calculates viscosity for temperature dependent (only) viscosity law.
+ * Calculates linear (i.e., temperature dependent) viscosity term from
+ * Arrhenius relationship.
  *
  *   visc (T) = exp (E * (0.5 - T))
+ *
+ *   E --- activation energy > 0
+ *   T --- temperature in [0,1]
  */
- double
-slabs_visc_temp_fn (const double visc_temp_decay, const double temp)
+static double
+rhea_viscosity_linear_arrhenius (const double activation_energy,
+                                 const double temp)
 {
-  return exp (visc_temp_decay * (0.5 - temp));
+  return exp (activation_energy * (0.5 - temp));
 }
 
 /**
- * Computes temperature dependent viscosity at a node.
+ * Computes linear viscosity at one node.  It incorporates temperature
+ * dependence and weak zones.
  */
-static inline double
-slabs_visc_temp_node (const double temp, const double weak,
-                      const double scaling, const double visc_temp_decay,
-                      slabs_physics_options_t *physics_options,
-                      const int restrict_to_bounds)
+static double
+rhea_viscosity_linear_node (const double temp,
+                            const double weak,
+                            const double scaling,
+                            const double activation_energy,
+                            rhea_viscosity_options_t *opt,
+                            const int restrict_to_bounds)
 {
-  const slabs_viscosity_model_t  viscosity_model =
-                        physics_options->viscosity_model_type;
-  const double        visc_min = physics_options->viscosity_min;
-  const double        visc_max = physics_options->viscosity_max;
-  const double        visc_temp_max = physics_options->viscosity_temp_max;
+  const rhea_viscosity_model_t  model = opt->model;
+  const double        visc_min = opt->min;
+  const double        visc_max = opt->max;
 
   double              visc_node;
 
-  /* compute viscosity */
-  visc_node = scaling * slabs_visc_temp_fn (visc_temp_decay, temp);
+  /* compute viscosity from Arrhenius relationship */
+  visc_node = scaling * rhea_viscosity_linear_arrhenius (activation_energy,
+                                                         temp);
 
   /* apply weak zone and restrict to bounds */
-  switch (viscosity_model) {
-  case SL_VISCOSITY_MODEL_WYUL:
-  case SL_VISCOSITY_MODEL_UWYUL:
-    /* restrict viscosity to upper bound for temp. dep. viscosity */
-    if (   viscosity_model == SL_VISCOSITY_MODEL_UWYUL && restrict_to_bounds
-        && 0.0 < visc_temp_max && visc_temp_max < visc_node ) {
-      visc_node = visc_temp_max;
+  switch (model) {
+  case RHEA_VISCOSITY_MODEL_UWYL:
+    /* restrict viscosity to upper bound */
+    if (restrict_to_bounds && 0.0 < visc_max && visc_max < visc_node) {
+      visc_node = visc_max;
     }
 
     /* multiply by weak zone */
     visc_node *= weak;
 
-    /* restrict viscosity to upper bound */
-    if ( restrict_to_bounds && 0.0 < visc_max && visc_max < visc_node ) {
-      visc_node = visc_max;
-    }
-
     /* restrict viscosity to lower bound */
-    if ( restrict_to_bounds && 0.0 < visc_min && visc_node < visc_min ) {
+    if (restrict_to_bounds && 0.0 < visc_min && visc_node < visc_min) {
       visc_node = visc_min;
     }
     break;
 
-  case SL_VISCOSITY_MODEL_UWYL:
-  case SL_VISCOSITY_MODEL_UYWL:
-  case SL_VISCOSITY_MODEL_UYWL_SHIFT:
-  case SL_VISCOSITY_MODEL_UWL_IIE_REG:
+  case RHEA_VISCOSITY_MODEL_UWYL_LREG:
+  case RHEA_VISCOSITY_MODEL_UWYL_SHIFT_LREG:
     /* restrict viscosity to upper bound */
-    if ( restrict_to_bounds && 0.0 < visc_max && visc_max < visc_node ) {
+    if (restrict_to_bounds && 0.0 < visc_max && visc_max < visc_node) {
       visc_node = visc_max;
     }
 
@@ -72,141 +70,123 @@ slabs_visc_temp_node (const double temp, const double weak,
     visc_node *= weak;
 
     /* restrict viscosity to lower bound */
-    if ( restrict_to_bounds && 0.0 < visc_min && visc_node < visc_min ) {
-      visc_node = visc_min;
-    }
-    break;
-
-  case SL_VISCOSITY_MODEL_UWYL_LREG:
-  case SL_VISCOSITY_MODEL_UWYL_SHIFT_LREG:
-    /* restrict viscosity to upper bound */
-    if ( restrict_to_bounds && 0.0 < visc_max && visc_max < visc_node ) {
-      visc_node = visc_max;
-    }
-
-    /* multiply by weak zone */
-    visc_node *= weak;
-
-    /* restrict viscosity to lower bound */
-    if ( restrict_to_bounds && 0.0 < visc_min ) {
+    if (restrict_to_bounds && 0.0 < visc_min) {
       visc_node += visc_min;
     }
     break;
 
-  default: /* unknown viscosity model type */
-    YMIR_ABORT_NOT_REACHED ();
+  default: /* unknown viscosity model */
+    RHEA_ABORT_NOT_REACHED ();
   }
 
-  /* return temperature dependent viscosity */
+  /* return viscosity */
   return visc_node;
 }
 
 /**
- * Computes temperature dependent viscosity in an element.
+ * Computes linear viscosity in an element.
  */
-void
-slabs_visc_temp_elem (sc_dmatrix_t *visc_el_mat,
-                      const double *x, const double *y, const double *z,
-                      const int *Vmask,
-                      const sc_dmatrix_t *temp_el_mat,
-                      const sc_dmatrix_t *weak_el_mat,
-                      slabs_physics_options_t *physics_options,
-                      const int restrict_to_bounds)
+static void
+rhea_viscosity_linear_elem (sc_dmatrix_t *visc_el_mat,
+                            const double *_sc_restrict x,
+                            const double *_sc_restrict y,
+                            const double *_sc_restrict z,
+                            const int *Vmask,
+                            const sc_dmatrix_t *temp_el_mat,
+                            const sc_dmatrix_t *weak_el_mat,
+                            rhea_viscosity_options_t *opt,
+                            const int restrict_to_bounds)
 {
-  const double        upper_mantle_radius =
-                        physics_options->viscosity_upper_mantle_radius;
-  const double        transition_zone =
-                        physics_options->viscosity_lower_upper_transition_zone;
+  rhea_domain_options_t  *domain_options = opt->domain_options;
+  const double        lm_um_interface_radius =
+                        domain_options->lm_um_interface_radius;
+  const double        transition_width =
+                        domain_options->lm_um_interface_smooth_transition_width;
+  const double        um_scaling = opt->upper_mantle_scaling;
+  const double        um_activ_energy = opt->upper_mantle_activation_energy;
+  const double        lm_scaling = opt->lower_mantle_scaling;
+  const double        lm_activ_energy = opt->lower_mantle_activation_energy;
   const int           n_nodes_per_el = visc_el_mat->m;
   const double       *temp_el_data = temp_el_mat->e[0];
+
+  double              scaling, activ_energy;
   double             *visc_el_data = visc_el_mat->e[0];
-  double              scaling, visc_temp_decay;
   int                 nodeid;
 
-  /* check input parameters */
-  YMIR_ASSERT (visc_el_mat->n == 1);
-  YMIR_ASSERT (temp_el_mat != NULL);
-  YMIR_ASSERT (temp_el_mat->m == n_nodes_per_el);
-  YMIR_ASSERT (temp_el_mat->n == visc_el_mat->n);
-  YMIR_ASSERT (weak_el_mat == NULL || weak_el_mat->m == n_nodes_per_el);
-  YMIR_ASSERT (weak_el_mat == NULL || weak_el_mat->n == visc_el_mat->n);
-  YMIR_ASSERT (sc_dmatrix_is_valid (temp_el_mat));
+  /* check input */
+  RHEA_ASSERT (visc_el_mat->n == 1);
+  RHEA_ASSERT (temp_el_mat != NULL);
+  RHEA_ASSERT (temp_el_mat->m == visc_el_mat->m);
+  RHEA_ASSERT (temp_el_mat->n == visc_el_mat->n);
+  RHEA_ASSERT (sc_dmatrix_is_valid (temp_el_mat));
+  RHEA_ASSERT (weak_el_mat == NULL || weak_el_mat->m == visc_el_mat->m);
+  RHEA_ASSERT (weak_el_mat == NULL || weak_el_mat->n == visc_el_mat->n);
+  RHEA_ASSERT (weak_el_mat == NULL || sc_dmatrix_is_valid (weak_el_mat));
 
   /* set parameters depending on location in lower or upper mantle */
-  if (slabs_physics_elem_in_upper_mantle (x, y, z, Vmask, physics_options)) {
-    /* set upper mantle parameters */
-    scaling = physics_options->viscosity_scaling;
-    visc_temp_decay = physics_options->viscosity_temp_decay;
+  if (rhea_domain_elem_is_in_upper_mantle (x, y, z, Vmask, domain_options)) {
+    scaling = um_scaling;
+    activ_energy = um_activ_energy;
   }
   else {
-    /* set lower mantle parameters */
-    scaling = physics_options->viscosity_lower_mantle_scaling;
-    visc_temp_decay = physics_options->viscosity_lower_mantle_temp_decay;
+    scaling = lm_scaling;
+    activ_energy = lm_activ_energy;
   }
 
-  /* compute viscosity in this element */
-  for (nodeid = 0; nodeid < n_nodes_per_el; nodeid++) { /* loop over all
-                                                         * nodes */
-    const double        r =
-      slabs_compute_radius (x[nodeid], y[nodeid], z[nodeid], physics_options);
+  /* compute viscosity at each node of this element */
+  for (nodeid = 0; nodeid < n_nodes_per_el; nodeid++) {
+    const double        r = rhea_domain_compute_radius (x[nodeid], y[nodeid],
+                                                        z[nodeid],
+                                                        domain_options);
     const double        temp = temp_el_data[nodeid];
-    double              weak;
+    const double        weak = ( weak_el_mat != NULL ?
+                                 weak_el_data[nodeid] : 1.0 );
 
     /* check temperature for valid range [0,1] */
-    YMIR_ASSERT (0.0 <= temp && temp <= 1.0);
-
-    /* set weak zone */
-    if (weak_el_mat != NULL) {
-      weak = weak_el_mat->e[nodeid][0];
-      YMIR_ASSERT (isfinite (weak));
-      YMIR_ASSERT (0.0 < weak && weak <= 1.0);
-    }
-    else {
-      weak = 1.0;
-    }
+    RHEA_ASSERT (0.0 <= temp && temp <= 1.0);
+    /* check weak zone for valid range (0,1] */
+    RHEA_ASSERT (weak_el_mat == NULL || (0.0 < weak && weak <= 1.0));
 
     /* compute viscosity */
-    if (upper_mantle_radius <= 0.0 || transition_zone <= 0.0 ||
-        transition_zone < fabs (r - upper_mantle_radius)) {
-      /* compute viscosity disregarding LM/UM transition zone */
-      visc_el_data[nodeid] = slabs_visc_temp_node (temp, weak, scaling,
-                                                   visc_temp_decay,
-                                                   physics_options,
-                                                   restrict_to_bounds);
+    if (lm_um_interface_radius <= 0.0 || transition_width <= 0.0 ||
+        transition_width < fabs (r - lm_um_interface_radius)) {
+      /* compute viscosity "sufficiently far" from LM/UM interface or with a
+       * discontinuous LM/UM interface */
+      visc_el_data[nodeid] = rhea_viscosity_linear_node (
+          temp, weak, scaling, activ_energy, opt, restrict_to_bounds);
     }
-    else {
-      const double        factor =
-        (transition_zone + (r - upper_mantle_radius)) / (2.0*transition_zone);
+    else { /* if close to LM/UM interface and must apply smoothing */
+      const double        c =
+        (transition_width + (r - lm_um_interface_radius)) /
+        (2.0 * transition_width);
       double              visc_lm, visc_um;
 
-      /* compute viscosity with LM/UM transition zone (convex combination) */
-      visc_lm = slabs_visc_temp_node (
-          temp, weak, physics_options->viscosity_lower_mantle_scaling,
-          physics_options->viscosity_lower_mantle_temp_decay,
-          physics_options, restrict_to_bounds);
-      visc_um = slabs_visc_temp_node (
-          temp, weak, physics_options->viscosity_scaling,
-          physics_options->viscosity_temp_decay,
-          physics_options, restrict_to_bounds);
-      visc_el_data[nodeid] = (1.0 - factor) * visc_lm + factor * visc_um;
+      /* compute viscosity with smooth transition at LM/UM interface (via a
+       * convex combination) */
+      visc_lm = rhea_viscosity_linear_node (
+          temp, weak, lm_scaling, lm_activ_energy, opt, restrict_to_bounds);
+      visc_um = rhea_viscosity_linear_node (
+          temp, weak, um_scaling, um_activ_energy, opt, restrict_to_bounds);
+      visc_el_data[nodeid] = (1.0 - c) * visc_lm + c * visc_um;
     }
 
     /* check viscosity for `nan`, `inf`, and positivity */
-    YMIR_ASSERT (isfinite (visc_el_data[nodeid]));
-    YMIR_ASSERT (0.0 < visc_el_data[nodeid]);
+    RHEA_ASSERT (isfinite (visc_el_data[nodeid]));
+    RHEA_ASSERT (0.0 < visc_el_data[nodeid]);
   }
 }
 
+#if 0
 /**
- * Computes the temperature dependent viscosity vector.
+ * Computes linear viscosity.
  */
 static void
-slabs_viscosity_linear (ymir_dvec_t *viscosity,
-                        slabs_stokes_state_t *state,
-                        slabs_physics_options_t *physics_options)
+rhea_viscosity_linear (ymir_vec_t *viscosity,
+                       slabs_stokes_state_t *state,
+                       rhea_viscosity_options_t *opt)
 {
-  ymir_cvec_t        *temp_vec = state->temp_vec;
-  ymir_dvec_t        *weak_vec = state->weak_vec;
+  ymir_vec_t         *temp_vec = state->temp_vec;
+  ymir_vec_t         *weak_vec = state->weak_vec;
   ymir_mesh_t        *mesh = viscosity->mesh;
   mangll_t           *mangll = mesh->ma;
   const mangll_locidx_t  n_elements = mesh->cnodes->K;
@@ -249,7 +229,7 @@ slabs_viscosity_linear (ymir_dvec_t *viscosity,
 
     /* compute temperature dependent viscosity (restrict to bounds) */
     slabs_visc_temp_elem (visc_el_mat, x, y, z, mangll->refel->Vmask,
-                          temp_el_mat, weak_el_mat, physics_options, 1);
+                          temp_el_mat, weak_el_mat, opt, 1);
 
     /* set viscosity of this element */
     ymir_dvec_set_elem (viscosity, visc_el_mat, YMIR_STRIDE_NODE, elid,
@@ -265,3 +245,4 @@ slabs_viscosity_linear (ymir_dvec_t *viscosity,
   YMIR_FREE (z);
   YMIR_FREE (tmp_el);
 }
+#endif
