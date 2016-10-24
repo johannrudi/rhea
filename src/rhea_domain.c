1a535bad90c6db9b7f7af5592d1f18d64df347ae
Johann Rudi <johann@ices.utexas.edu>
2016-10-24 17:43:17 -0500

    rhea_domain: Add handling of boundary conditions.

--- src/rhea_domain.c
+++ src/rhea_domain.c
@@ -17,6 +17,8 @@
 #define RHEA_DOMAIN_DEFAULT_MANTLE_DEPTH (2866.95e3)
 #define RHEA_DOMAIN_DEFAULT_LM_UM_INTERFACE_DEPTH (660.0e3)
 #define RHEA_DOMAIN_DEFAULT_LM_UM_INTERFACE_SMOOTH_TRANSITION_WIDTH (0.0)
+#define RHEA_DOMAIN_DEFAULT_VELOCITY_BC_TYPE \
+  RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_ALL
 
 /* initialize options */
 char               *rhea_domain_shape_name = RHEA_DOMAIN_DEFAULT_SHAPE_NAME;
@@ -32,6 +34,8 @@ double              rhea_domain_lm_um_interface_depth =
   RHEA_DOMAIN_DEFAULT_LM_UM_INTERFACE_DEPTH;
 double              rhea_domain_lm_um_interface_smooth_transition_width =
   RHEA_DOMAIN_DEFAULT_LM_UM_INTERFACE_SMOOTH_TRANSITION_WIDTH;
+int                 rhea_domain_velocity_bc_type =
+  RHEA_DOMAIN_DEFAULT_VELOCITY_BC_TYPE;
 
 void
 rhea_domain_add_options (ymir_options_t * opt_sup)
@@ -70,6 +74,17 @@ rhea_domain_add_options (ymir_options_t * opt_sup)
     &rhea_domain_lm_um_interface_smooth_transition_width,
     RHEA_DOMAIN_DEFAULT_LM_UM_INTERFACE_SMOOTH_TRANSITION_WIDTH,
     "Width of smooth transition zone between lower and upper mantle [m]",
+
+  YMIR_OPTIONS_I, "velocity-boundary-condition", '\0',
+    &rhea_domain_velocity_bc_type, RHEA_DOMAIN_DEFAULT_VELOCITY_BC_TYPE,
+    "Velocity boundary condition: "
+    "0: Dirichlet all; "
+    "1: Dirichlet in normal direction; "
+    "2: Dirichlet norm (if shell: two inner points fixed for rotation inv); "
+    "3: Dirichlet all on inner sphere & Dirichlet norm on outer sphere; "
+    "4: Dirichlet all on side faces & Neumann on top, bottom faces; "
+    "5: Dirichlet norm on side faces & Dirichlet all on top, bottom faces; "
+    "6: Neumann all",
 
   YMIR_OPTIONS_END_OF_LIST);
   /* *INDENT-ON* */
@@ -465,6 +480,10 @@ rhea_domain_process_options (rhea_domain_options_t *opt)
     opt->lm_um_interface_smooth_transition_width = 0.0;
   }
 
+  /* set velocity boundary conditions */
+  opt->velocity_bc_type =
+    (rhea_domain_velocity_bc_t) rhea_domain_velocity_bc_type;
+
   /* print derived domain properties */
   RHEA_GLOBAL_INFO ("===================================================\n");
   RHEA_GLOBAL_INFOF ("%s\n", this_fn_name);
@@ -558,4 +577,293 @@ rhea_domain_elem_is_in_upper_mantle (const double *x, const double *y,
 
   /* return if radius of the element's center is in upper mantle */
   return (lm_um_interface_radius <= r_center);
+}
+
+/**
+ * Assigns boundary faces of to p4est-tree faces.
+ */
+static ymir_topidx_t *
+rhea_domain_get_tree_to_bf (p4est_connectivity_t *conn,
+                                    rhea_domain_shape_t domain_shape)
+{
+  const ymir_topidx_t ntrees = conn->num_trees;
+  ymir_topidx_t       treeid;
+  int                 faceid;
+  ymir_topidx_t      *tree_to_bf = RHEA_ALLOC (ymir_topidx_t, 6 * ntrees);
+
+  switch (domain_shape) {
+  case RHEA_DOMAIN_CUBE:
+  case RHEA_DOMAIN_CUBE_SPHERICAL:
+  case RHEA_DOMAIN_BOX:
+  case RHEA_DOMAIN_BOX_SPHERICAL:
+    for (treeid = 0; treeid < ntrees; treeid++) { /* loop over all trees */
+      /* set sides */
+      for (faceid = 0; faceid < 4; faceid++) { /* loop over all side faces */
+        if (conn->tree_to_tree[6 * treeid + faceid] == treeid) {
+          switch (faceid) {
+          case 0:
+            tree_to_bf[6 * treeid + faceid] = RHEA_DOMAIN_BOUNDARY_FACE_SIDE1;
+            break;
+          case 1:
+            tree_to_bf[6 * treeid + faceid] = RHEA_DOMAIN_BOUNDARY_FACE_SIDE2;
+            break;
+          case 2:
+            tree_to_bf[6 * treeid + faceid] = RHEA_DOMAIN_BOUNDARY_FACE_SIDE3;
+            break;
+          case 3:
+            tree_to_bf[6 * treeid + faceid] = RHEA_DOMAIN_BOUNDARY_FACE_SIDE4;
+            break;
+          default: /* unknown face type */
+            RHEA_ABORT_NOT_REACHED ();
+          }
+        }
+        else {
+          tree_to_bf[6 * treeid + faceid] = RHEA_DOMAIN_BOUNDARY_FACE_NONE;
+        }
+      }
+      /* set top and bottom */
+      tree_to_bf[6 * treeid + 4] = RHEA_DOMAIN_BOUNDARY_FACE_BASE;
+      tree_to_bf[6 * treeid + 5] = RHEA_DOMAIN_BOUNDARY_FACE_TOP;
+    }
+    break;
+
+  case RHEA_DOMAIN_SHELL:
+    for (treeid = 0; treeid < ntrees; treeid++) { /* loop over all trees */
+      /* set sides */
+      for (faceid = 0; faceid < 4; faceid++) { /* loop over all side faces */
+        tree_to_bf[6 * treeid + faceid] = RHEA_DOMAIN_BOUNDARY_FACE_NONE;
+      }
+      /* set top and bottom */
+      tree_to_bf[6 * treeid + 4] = RHEA_DOMAIN_BOUNDARY_FACE_BASE;
+      tree_to_bf[6 * treeid + 5] = RHEA_DOMAIN_BOUNDARY_FACE_TOP;
+    }
+    break;
+
+  default: /* unknown domain shape */
+    RHEA_ABORT_NOT_REACHED ();
+  }
+
+  return tree_to_bf;
+}
+
+rhea_domain_boundary_t *
+rhea_domain_boundary_new (p4est_t *p4est, rhea_domain_options_t *opt)
+{
+  rhea_domain_boundary_t  *boundary;
+
+  /* create new object */
+  boundary = RHEA_ALLOC (rhea_domain_boundary_t, 1);
+
+  /* set element to boundary function */
+  boundary->e_to_fm_fn = ymir_mesh_e_to_fm_tag;
+
+  /* set boundary faces */
+  boundary->tree_to_bf = rhea_domain_get_tree_to_bf (p4est->connectivity,
+                                                     opt->shape);
+
+  /* return boundary */
+  return boundary;
+}
+
+void
+rhea_domain_boundary_destroy (rhea_domain_boundary_t *boundary)
+{
+  RHEA_FREE (boundary->tree_to_bf);
+  RHEA_FREE (boundary);
+}
+
+/**
+ *
+ */
+static ymir_dir_code_t
+rhea_domain_vel_dir_fn_box (double X, double Y, double Z,
+                            double nx, double ny, double nz,
+                            ymir_topidx_t face, ymir_locidx_t node_id,
+                            void *data)
+{
+  int                 vel_bc_type = *((int *) data);
+
+  switch (vel_bc_type) {
+  case RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_ALL:
+    /* set Dirichlet in all directions for all points */
+    return YMIR_VEL_DIRICHLET_ALL;
+  case RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_NORM:
+    /* set Dirichlet in normal direction for all points */
+    return YMIR_VEL_DIRICHLET_NORM;
+  case RHEA_DOMAIN_VELOCITY_BC_DIR_SIDES_NEU_TB:
+    if ( (face != RHEA_DOMAIN_BOUNDARY_FACE_BASE) &&
+         (face != RHEA_DOMAIN_BOUNDARY_FACE_TOP) ) {
+      /* set Dirichlet in all directions on side faces */
+      return YMIR_VEL_DIRICHLET_ALL;
+    }
+    else {
+      /* bottom and top faces will get Neumann BC's */
+      return YMIR_VEL_DIRICHLET_NONE;
+    }
+  case RHEA_DOMAIN_VELOCITY_BC_DIR_NORM_SIDES_DIR_ALL_TB:
+    if ( (face != RHEA_DOMAIN_BOUNDARY_FACE_BASE) &&
+         (face != RHEA_DOMAIN_BOUNDARY_FACE_TOP) ) {
+      /* set Dirichlet in normal direction on side faces */
+      return YMIR_VEL_DIRICHLET_NORM;
+    }
+    else {
+      /* set Dirichlet in all directions on bottom and top faces */
+      return YMIR_VEL_DIRICHLET_ALL;
+    }
+  case RHEA_DOMAIN_VELOCITY_BC_NEUMANN_ALL:
+    /* set Neumann for all points */
+    return YMIR_VEL_DIRICHLET_NONE;
+  default: /* unknown boundary condition */
+    RHEA_ABORT_NOT_REACHED ();
+  }
+}
+
+/**
+ *
+ */
+static ymir_dir_code_t
+rhea_domain_vel_dir_fn_shell (double X, double Y, double Z,
+                              double nx, double ny, double nz,
+                              ymir_topidx_t face, ymir_locidx_t node_id,
+                              void *data)
+{
+  int                 vel_bc_type = *((int *) data);
+
+  switch (vel_bc_type) {
+  case RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_ALL:
+    /* set Dirichlet BC in all directions for all points */
+    return YMIR_VEL_DIRICHLET_ALL;
+  case RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_NORM:
+  case RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_NORM_FIXDOF:
+    /* set Dirichlet BC in normal direction for all points */
+    return YMIR_VEL_DIRICHLET_NORM;
+  case RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_ALL_INNER:
+    if (face == RHEA_DOMAIN_BOUNDARY_FACE_TOP) {
+      /* set Dirichlet BC in normal direction on outer sphere of a shell */
+      return YMIR_VEL_DIRICHLET_NORM;
+    }
+    else {
+      /* set Dirichlet BC in all direction on inner sphere of a shell */
+      return YMIR_VEL_DIRICHLET_ALL;
+    }
+  case RHEA_DOMAIN_VELOCITY_BC_NEUMANN_ALL:
+    /* set Neumann for all points */
+    return YMIR_VEL_DIRICHLET_NONE;
+  default: /* unknown boundary condition */
+    RHEA_ABORT_NOT_REACHED ();
+  }
+}
+
+/**
+ * Callback function for `slabs_shell_vel_dir_restrict_dof` below.
+ */
+static void
+rhea_domain_vel_dir_shell_restrict_dof_fn (double *dummy,
+                                           double X, double Y, double Z,
+                                           double nx, double ny, double nz,
+                                           ymir_topidx_t face,
+                                           ymir_locidx_t node_id,
+                                           void *data)
+{
+  ymir_vel_dir_t     *vel_dir = data;
+
+  /* set one point to all Dirichlet */
+  if (face == RHEA_DOMAIN_BOUNDARY_FACE_BASE && fabs (nz - 1.0) < SC_EPS) {
+    ymir_mesh_t        *mesh = vel_dir->mesh;
+    ymir_face_mesh_t   *fmesh = &mesh->fmeshes[face];
+    ymir_locidx_t       vcnid = fmesh->CntoVCn[node_id];
+    double             *vn = ymir_cvec_index (vel_dir->nvec, vcnid, 0);
+
+    /* set Dirichlet in all directions */
+    vel_dir->rank[vcnid] = YMIR_VEL_DIRICHLET_ALL;
+    vn[0] = 0;
+    vn[1] = 0;
+    vn[2] = 0;
+
+    RHEA_INFOF ("Set boundary node %d with coord. (%g %g %g) "
+                "and normal (%g %g %g) to all Dirichlet\n",
+                node_id, X, Y, Z, nx, ny, nz);
+  }
+
+  /* set another point to Dirichlet in normal and one tangential direction */
+  if (face == RHEA_DOMAIN_BOUNDARY_FACE_BASE && fabs (ny - 1.0) < SC_EPS) {
+    ymir_mesh_t        *mesh = vel_dir->mesh;
+    ymir_face_mesh_t   *fmesh = &mesh->fmeshes[face];
+    ymir_locidx_t       vcnid = fmesh->CntoVCn[node_id];
+    double             *vn = ymir_cvec_index (vel_dir->nvec, vcnid, 0);
+
+    /* set the way the vector `vn` is interpreted */
+    vel_dir->rank[vcnid] = YMIR_VEL_DIRICHLET_TANG;
+
+    /* set Dirichlet in y- and z-direction */
+    vn[0] = 1;
+    vn[1] = 0;
+    vn[2] = 0;
+
+    RHEA_INFOF ("Set boundary node %d with coord. (%g %g %g) "
+                "and normal (%g %g %g) to Dirichlet in normal "
+                "and one tangential direction\n",
+                node_id, X, Y, Z, nx, ny, nz);
+  }
+}
+
+/**
+ *
+ */
+static void
+rhea_domain_vel_dir_shell_restrict_dof (ymir_vel_dir_t * vel_dir)
+{
+  ymir_mesh_t        *mesh = vel_dir->mesh;
+  const ymir_topidx_t n_faces = mesh->num_face_meshes;
+  ymir_topidx_t       faceid;
+
+  for (faceid = 0; faceid < n_faces; faceid++) {
+    ymir_vec_t         *dummy_face_cvec;
+
+    /* create dummy vector for current face */
+    dummy_face_cvec = ymir_face_cvec_new (mesh, faceid, 1);
+
+    /* set additional Dirichlet boundary conditions for current face */
+    ymir_face_cvec_set_function (
+        dummy_face_cvec, rhea_domain_vel_dir_shell_restrict_dof_fn, vel_dir);
+
+    /* destroy vector */
+    ymir_vec_destroy (dummy_face_cvec);
+  }
+}
+
+ymir_vel_dir_t *
+rhea_domain_create_velocity_dirichlet_bc (ymir_mesh_t *mesh,
+                                          ymir_vec_t *dirscal,
+                                          void *data)
+{
+  rhea_domain_options_t  *opt = data;
+  int                 vel_bc_type = (int) opt->velocity_bc_type;
+  ymir_vel_dir_t     *vel_dir;
+
+  /* create velocity Dirichlet BC's */
+  switch (opt->shape) {
+  case RHEA_DOMAIN_CUBE:
+  case RHEA_DOMAIN_CUBE_SPHERICAL:
+  case RHEA_DOMAIN_BOX:
+  case RHEA_DOMAIN_BOX_SPHERICAL:
+    vel_dir = ymir_vel_dir_new_from_faces (
+        mesh, NULL, rhea_domain_vel_dir_fn_box, &vel_bc_type, dirscal);
+    break;
+  case RHEA_DOMAIN_SHELL:
+    vel_dir = ymir_vel_dir_new_from_faces (
+        mesh, NULL, rhea_domain_vel_dir_fn_shell, &vel_bc_type, dirscal);
+    if (RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_NORM_FIXDOF == vel_bc_type) {
+      rhea_domain_vel_dir_shell_restrict_dof (vel_dir);
+    }
+    break;
+  default: /* unknown domain shape */
+    RHEA_ABORT_NOT_REACHED ();
+  }
+
+  /* set zero Dirichlet boundary */
+  vel_dir->nonzero = 0;
+
+  /* return BC's */
+  return vel_dir;
 }
--- src/rhea_domain.h
+++ src/rhea_domain.h
@@ -4,7 +4,11 @@
 #ifndef RHEA_DOMAIN_H
 #define RHEA_DOMAIN_H
 
+#include <p4est_to_p8est.h>
+#include <p8est.h>
 #include <ymir_options.h>
+#include <ymir_mesh.h>
+#include <ymir_velocity_dirichlet.h>
 
 /* enumerator for domain shapes */
 typedef enum
@@ -16,6 +20,57 @@ typedef enum
   RHEA_DOMAIN_BOX_SPHERICAL
 }
 rhea_domain_shape_t;
+
+/* enumerator for boundary faces */
+typedef enum
+{
+  RHEA_DOMAIN_BOUNDARY_FACE_NONE = -1,
+  RHEA_DOMAIN_BOUNDARY_FACE_BASE = 0,
+  RHEA_DOMAIN_BOUNDARY_FACE_TOP,
+  RHEA_DOMAIN_BOUNDARY_FACE_SIDE1,
+  RHEA_DOMAIN_BOUNDARY_FACE_SIDE2,
+  RHEA_DOMAIN_BOUNDARY_FACE_SIDE3,
+  RHEA_DOMAIN_BOUNDARY_FACE_SIDE4
+}
+rhea_domain_boundary_face_t;
+
+/* enumerator for velocity boundray conditions */
+typedef enum
+{
+  /* Dirichlet all */
+  RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_ALL = 0,
+
+  /* Dirchlet in normal direction */
+  RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_NORM = 1,
+
+  /* Dirchlet norm (if shell: fix two points on inner sphere for rotation
+   * invariance) */
+  RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_NORM_FIXDOF = 2,
+
+  /* Dirichlet all on inner sphere and Dirichlet norm on outer sphere of a shell
+   * (for shell domain only) */
+  RHEA_DOMAIN_VELOCITY_BC_DIRICHLET_ALL_INNER = 3,
+
+  /* Dirichlet all on side faces and Neumann on top and bottom faces
+   * (for rectangular domains only) */
+  RHEA_DOMAIN_VELOCITY_BC_DIR_SIDES_NEU_TB = 4,
+
+  /* Dirichlet norm on side faces and Dirichlet all on top and bottom faces
+   * (for rectangular domains only) */
+  RHEA_DOMAIN_VELOCITY_BC_DIR_NORM_SIDES_DIR_ALL_TB = 5,
+
+  /* Neumann */
+  RHEA_DOMAIN_VELOCITY_BC_NEUMANN_ALL = 6
+}
+rhea_domain_velocity_bc_t;
+
+/* boundary information */
+typedef struct rhea_domain_boundary
+{
+  ymir_mesh_e_to_fm_t e_to_fm_fn;
+  ymir_topidx_t      *tree_to_bf; /* data for `e_to_fm_fn` */
+}
+rhea_domain_boundary_t;
 
 /* options & properties of a computational domain */
 typedef struct rhea_domain_options
@@ -32,6 +87,9 @@ typedef struct rhea_domain_options
    * which causes discontinuous material properties */
   double              lm_um_interface_radius;
   double              lm_um_interface_smooth_transition_width;
+
+  /* velocity boundary conditions */
+  rhea_domain_velocity_bc_t  velocity_bc_type;
 
   /* properties of the domain (reference coordinate system) */
   double              x_min;
@@ -77,5 +135,25 @@ int                 rhea_domain_elem_is_in_upper_mantle (
                                                   const double *z,
                                                   const int *Vmask,
                                                   rhea_domain_options_t *opt);
+
+/**
+ * Creates a new boundary object.
+ */
+rhea_domain_boundary_t *rhea_domain_boundary_new (p4est_t *p4est,
+                                                  rhea_domain_options_t *opt);
+
+/**
+ * Destroys a boundary object.
+ */
+void                rhea_domain_boundary_destroy (
+                                            rhea_domain_boundary_t *boundary);
+
+/**
+ * Creates Dirichlet boundary conditions.
+ */
+ymir_vel_dir_t     *rhea_domain_create_velocity_dirichlet_bc (
+                                                          ymir_mesh_t *mesh,
+                                                          ymir_vec_t *dirscal,
+                                                          void *data);
 
 #endif /* RHEA_DOMAIN_H */
