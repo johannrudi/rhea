250c31d72211a4e6ea8fa1cc6ef18a261fbc023e
Johann Rudi <johann@ices.utexas.edu>
2016-11-01 11:35:38 -0500

    rhea: Include more headers.

--- src/rhea.h
+++ src/rhea.h
@@ -9,7 +9,9 @@
 #include <rhea_discretization.h>
 #include <rhea_temperature.h>
 #include <rhea_viscosity.h>
+#include <rhea_velocity.h>
 #include <rhea_stokes_problem.h>
+#include <rhea_vtk.h>
 
 /**
  * Defines all rhea options and adds them as sub-options.
