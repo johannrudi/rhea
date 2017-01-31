c09b64f7454e88c72700ab34ec55a12486ce9faa
Johann Rudi <johann@ices.utexas.edu>
2017-01-31 17:56:13 -0600

    rhea: Add options of Newton solver.

--- src/rhea.h
+++ src/rhea.h
@@ -34,7 +34,8 @@ void                rhea_process_options_all (
                               rhea_domain_options_t *domain_options,
                               rhea_temperature_options_t *temperature_options,
                               rhea_viscosity_options_t *viscosity_options,
-                              rhea_discretization_options_t *discr_options);
+                              rhea_discretization_options_t *discr_options,
+                              rhea_newton_options_t *newton_options);
 
 /**
  * Processes a subset of options and stores them.
