b1863ccbd722d2d977db76ecc1909bd8853833d3
Johann Rudi <johann@ices.utexas.edu>
2017-01-31 17:56:59 -0600

    Collide: Update interface.

--- example/collide/collide.c
+++ example/collide/collide.c
@@ -876,7 +876,7 @@ collide_setup_stokes (rhea_stokes_problem_t **stokes_problem,
   *stokes_problem = rhea_stokes_problem_new (
       temperature, weakzone, rhs_vel_nonzero_dirichlet,
       ymir_mesh, press_elem,
-      domain_options, temp_options, visc_options);
+      domain_options, temp_options, visc_options, NULL /* solver options */);
   rhea_stokes_problem_setup_solver (*stokes_problem);
 
   if (vtk_write_input_path != NULL)
@@ -1024,6 +1024,7 @@ main (int argc, char **argv)
   rhea_temperature_options_t    temp_options;
   rhea_viscosity_options_t      visc_options;
   rhea_discretization_options_t discr_options;
+  rhea_newton_options_t         newton_options;
   /* collide options */
   int                 vel_dir_bc;
   double              wallslide_vel;
@@ -1168,7 +1169,8 @@ main (int argc, char **argv)
   /* print & process options */
   ymir_options_print_summary (SC_LP_INFO, opt);
   rhea_process_options_all (&domain_options, &temp_options,
-                            &visc_options, &discr_options);
+                            &visc_options, &discr_options,
+                            &newton_options);
 
   /*
    * Setup Mesh
