49e2c7005adf4979b283e798549e74961fbf03ab
Johann Rudi <johann@ices.utexas.edu>
2017-10-20 16:22:17 -0500

    rhea_amr: Add decoding of refinement names into types.  Add refinement with respect to depth.

--- src/rhea_amr.c
+++ src/rhea_amr.c
@@ -3,6 +3,37 @@
 
 #include <rhea_amr.h>
 
+const char         *rhea_amr_p4est_refine_name[RHEA_AMR_P4EST_REFINE_N] =
+{
+  "uniform",
+  "half",
+  "depth"
+};
+
+rhea_amr_p4est_refine_t
+rhea_amr_p4est_refine_decode (const char * name)
+{
+  const int           n_types = (int) RHEA_AMR_P4EST_REFINE_N;
+  int                 typeid;
+  rhea_amr_p4est_refine_t type;
+
+  /* exit if nothing to do */
+  if (name == NULL) {
+    return RHEA_AMR_P4EST_REFINE_NONE;
+  }
+
+  /* match name to a type */
+  type = RHEA_AMR_P4EST_REFINE_UNKNOWN;
+  for (typeid = 0; typeid < n_types; typeid++) {
+    if (!strcmp (name, rhea_amr_p4est_refine_name[typeid])) {
+      type = (rhea_amr_p4est_refine_t) typeid;
+    }
+  }
+
+  /* return refinement type */
+  return type;
+}
+
 int
 rhea_amr_p4est_refine_half (p4est_t * p4est, p4est_topidx_t which_tree,
                             p4est_quadrant_t * quadrant)
@@ -13,4 +44,26 @@ rhea_amr_p4est_refine_half (p4est_t * p4est, p4est_topidx_t which_tree,
   else {
     return 0;
   }
+}
+
+int
+rhea_amr_p4est_refine_depth (p4est_t * p4est, p4est_topidx_t which_tree,
+                              p4est_quadrant_t * quadrant)
+{
+  rhea_amr_p4est_refine_depth_data_t  *d = p4est->user_pointer;
+  const int          *depth = d->depth;
+  const int           quad_depth = P4EST_ROOT_LEN - quadrant->z;
+  int                 k;
+
+  if ((int) quadrant->level < d->level_min) {
+    return 1;
+  }
+
+  for (k = 0; k < d->count; k++) {
+    if (quad_depth <= depth[k] && (int) quadrant->level < d->level_min+k+1) {
+      return 1;
+    }
+  }
+
+  return 0;
 }
--- src/rhea_amr.h
+++ src/rhea_amr.h
@@ -3,8 +3,34 @@
 
 #include <rhea_domain.h>
 
+/* types of refinement when creating a new p4est mesh */
+typedef enum
+{
+  RHEA_AMR_P4EST_REFINE_UNKNOWN = -2,
+  RHEA_AMR_P4EST_REFINE_NONE    = -1,
+  RHEA_AMR_P4EST_REFINE_UNIFORM =  0, /* default, does not have a refine fn. */
+  RHEA_AMR_P4EST_REFINE_HALF,
+  RHEA_AMR_P4EST_REFINE_DEPTH,
+  RHEA_AMR_P4EST_REFINE_N
+}
+rhea_amr_p4est_refine_t;
+
+rhea_amr_p4est_refine_t rhea_amr_p4est_refine_decode (const char * name);
+
 int                 rhea_amr_p4est_refine_half (p4est_t * p4est,
                                                 p4est_topidx_t which_tree,
                                                 p4est_quadrant_t * quadrant);
+
+typedef struct rhea_amr_p4est_refine_depth_data
+{
+  int                *depth;
+  int                 count;
+  int                 level_min;
+}
+rhea_amr_p4est_refine_depth_data_t;
+
+int                 rhea_amr_p4est_refine_depth (p4est_t * p4est,
+                                                 p4est_topidx_t which_tree,
+                                                 p4est_quadrant_t * quadrant);
 
 #endif /* RHEA_AMR_H */
