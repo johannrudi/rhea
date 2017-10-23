a0da5b84fd27552d2294fe491c92981c0636ed64
Johann Rudi <johann@ices.utexas.edu>
2017-10-23 18:41:11 -0500

    rhea_amr: Create general AMR function.

--- src/rhea_amr.c
+++ src/rhea_amr.c
@@ -3,19 +3,21 @@
 
 #include <rhea_amr.h>
 #include <rhea_base.h>
+#include <rhea_discretization.h>
+#include <p8est_bits.h>
 #include <p8est_extended.h>
 
-#define RHEA_AMR_P4EST_INIT_FN NULL
 #define RHEA_AMR_P4EST_REPLACE_FN NULL
+#define RHEA_AMR_P4EST_PARTITION_WEIGHT_FN NULL
 
 /* default options */
-#define RHEA_AMR_DEFAULT_INIT_P4EST_REFINE "uniform"
-#define RHEA_AMR_DEFAULT_INIT_P4EST_REFINE_DEPTH_M NULL
+#define RHEA_AMR_DEFAULT_INIT_REFINE_NAME "uniform"
+#define RHEA_AMR_DEFAULT_INIT_REFINE_DEPTH_M NULL
 
-char               *rhea_amr_init_p4est_refine =
-  RHEA_AMR_DEFAULT_INIT_P4EST_REFINE;
-char               *rhea_amr_init_p4est_refine_depth_m =
-  RHEA_AMR_DEFAULT_INIT_P4EST_REFINE_DEPTH_M;
+char               *rhea_amr_init_refine_name =
+  RHEA_AMR_DEFAULT_INIT_REFINE_NAME;
+char               *rhea_amr_init_refine_depth_m =
+  RHEA_AMR_DEFAULT_INIT_REFINE_DEPTH_M;
 
 void
 rhea_amr_add_options (ymir_options_t * opt_sup)
@@ -26,12 +28,11 @@ rhea_amr_add_options (ymir_options_t * opt_sup)
   /* *INDENT-OFF* */
   ymir_options_addv (opt,
 
-  YMIR_OPTIONS_S, "init-p4est-refine", '\0',
-    &(rhea_amr_init_p4est_refine), RHEA_AMR_DEFAULT_INIT_P4EST_REFINE,
+  YMIR_OPTIONS_S, "init-refine", '\0',
+    &(rhea_amr_init_refine_name), RHEA_AMR_DEFAULT_INIT_REFINE_NAME,
     "Refinement of new/initial mesh: uniform, half, radial",
-  YMIR_OPTIONS_S, "init-p4est-refine-depth", '\0',
-    &(rhea_amr_init_p4est_refine_depth_m),
-    RHEA_AMR_DEFAULT_INIT_P4EST_REFINE_DEPTH_M,
+  YMIR_OPTIONS_S, "init-refine-depth", '\0',
+    &(rhea_amr_init_refine_depth_m), RHEA_AMR_DEFAULT_INIT_REFINE_DEPTH_M,
     "Refinement 'depth': Sorted list of depths [m] (put shallowest at last)",
 
   YMIR_OPTIONS_END_OF_LIST);
@@ -43,46 +44,46 @@ rhea_amr_add_options (ymir_options_t * opt_sup)
 }
 
 /******************************************************************************
- * Initial AMR for p4est Mesh
+ * Initial AMR for p4est
  *****************************************************************************/
 
 /* types of initial refinement for a p4est mesh */
 typedef enum
 {
-  RHEA_AMR_P4EST_REFINE_UNKNOWN = -2,
-  RHEA_AMR_P4EST_REFINE_NONE    = -1,
-  RHEA_AMR_P4EST_REFINE_UNIFORM =  0, /* default, does not have a refine fn. */
-  RHEA_AMR_P4EST_REFINE_HALF,
-  RHEA_AMR_P4EST_REFINE_DEPTH,
-  RHEA_AMR_P4EST_REFINE_N
+  RHEA_AMR_INIT_REFINE_UNKNOWN = -2,
+  RHEA_AMR_INIT_REFINE_NONE    = -1,
+  RHEA_AMR_INIT_REFINE_UNIFORM =  0, /* default, does not have a refine fn. */
+  RHEA_AMR_INIT_REFINE_HALF,
+  RHEA_AMR_INIT_REFINE_DEPTH,
+  RHEA_AMR_INIT_REFINE_N
 }
-rhea_amr_p4est_refine_t;
+rhea_amr_init_refine_t;
 
 /* name for types of initial refinement */
-const char         *rhea_amr_p4est_refine_name[RHEA_AMR_P4EST_REFINE_N] =
+const char         *rhea_amr_init_refine_name_list[RHEA_AMR_INIT_REFINE_N] =
 {
   "uniform",
   "half",
   "depth"
 };
 
-static rhea_amr_p4est_refine_t
-rhea_amr_p4est_refine_decode (const char * name)
+static rhea_amr_init_refine_t
+rhea_amr_init_refine_decode (const char * name)
 {
-  const int           n_types = (int) RHEA_AMR_P4EST_REFINE_N;
+  const int           n_types = (int) RHEA_AMR_INIT_REFINE_N;
   int                 typeid;
-  rhea_amr_p4est_refine_t type;
+  rhea_amr_init_refine_t type;
 
   /* exit if nothing to do */
   if (name == NULL) {
-    return RHEA_AMR_P4EST_REFINE_NONE;
+    return RHEA_AMR_INIT_REFINE_NONE;
   }
 
   /* match name to a type */
-  type = RHEA_AMR_P4EST_REFINE_UNKNOWN;
+  type = RHEA_AMR_INIT_REFINE_UNKNOWN;
   for (typeid = 0; typeid < n_types; typeid++) {
-    if (!strcmp (name, rhea_amr_p4est_refine_name[typeid])) {
-      type = (rhea_amr_p4est_refine_t) typeid;
+    if (!strcmp (name, rhea_amr_init_refine_name_list[typeid])) {
+      type = (rhea_amr_init_refine_t) typeid;
     }
   }
 
@@ -133,39 +134,39 @@ rhea_amr_p4est_refine_depth (p4est_t * p4est, p4est_topidx_t which_tree,
 }
 
 int
-rhea_amr_p4est_refine (p4est_t *p4est,
-                       const int level_min,
-                       const int level_max,
-                       rhea_domain_options_t *domain_options)
+rhea_amr_init_refine (p4est_t *p4est,
+                      const int level_min,
+                      const int level_max,
+                      rhea_domain_options_t *domain_options)
 {
   /* arguments for refinement */
-  const char         *refine_name = rhea_amr_init_p4est_refine;
-  rhea_amr_p4est_refine_t type;
+  const char         *refine_name = rhea_amr_init_refine_name;
+  rhea_amr_init_refine_t type;
   p4est_refine_t      refine_fn = NULL;
   void               *refine_data = NULL;
   int                 recursively = 0;
-  const p4est_init_t    init_fn = RHEA_AMR_P4EST_INIT_FN;
+  const p4est_init_t    init_fn = rhea_p4est_init_fn;
   const p4est_replace_t replace_fn = RHEA_AMR_P4EST_REPLACE_FN;
   /* arguments for partitioning */
   const int           partition_for_coarsening = 1;
-  p4est_weight_t      partition_weight_fn = NULL;
+  p4est_weight_t      partition_weight_fn = RHEA_AMR_P4EST_PARTITION_WEIGHT_FN;
 
   /* get type of refinement from name */
-  type = rhea_amr_p4est_refine_decode (refine_name);
+  type = rhea_amr_init_refine_decode (refine_name);
 
   /* set refinement function and data */
   switch (type) {
-  case RHEA_AMR_P4EST_REFINE_UNKNOWN:
+  case RHEA_AMR_INIT_REFINE_UNKNOWN:
     RHEA_GLOBAL_LERROR ("Unknown refinement type");
     break;
-  case RHEA_AMR_P4EST_REFINE_NONE:
-  case RHEA_AMR_P4EST_REFINE_UNIFORM:
+  case RHEA_AMR_INIT_REFINE_NONE:
+  case RHEA_AMR_INIT_REFINE_UNIFORM:
     /* no function necessary */
     break;
-  case RHEA_AMR_P4EST_REFINE_HALF:
+  case RHEA_AMR_INIT_REFINE_HALF:
     refine_fn = rhea_amr_p4est_refine_half;
     break;
-  case RHEA_AMR_P4EST_REFINE_DEPTH:
+  case RHEA_AMR_INIT_REFINE_DEPTH:
     {
       double             *depth_m = NULL;
       int                 count, k;
@@ -177,11 +178,11 @@ rhea_amr_p4est_refine (p4est_t *p4est,
 
       /* convert input string to double values */
       count = ymir_options_convert_string_to_double (
-          rhea_amr_init_p4est_refine_depth_m, &depth_m);
+          rhea_amr_init_refine_depth_m, &depth_m);
 
       /* skip refinement if depths were not provided */
       if (count == 0) {
-        type = RHEA_AMR_P4EST_REFINE_NONE;
+        type = RHEA_AMR_INIT_REFINE_NONE;
         break;
       }
 
@@ -236,11 +237,11 @@ rhea_amr_p4est_refine (p4est_t *p4est,
 
   /* destroy refinement data */
   switch (type) {
-  case RHEA_AMR_P4EST_REFINE_NONE:
-  case RHEA_AMR_P4EST_REFINE_UNIFORM:
-  case RHEA_AMR_P4EST_REFINE_HALF:
+  case RHEA_AMR_INIT_REFINE_NONE:
+  case RHEA_AMR_INIT_REFINE_UNIFORM:
+  case RHEA_AMR_INIT_REFINE_HALF:
     break;
-  case RHEA_AMR_P4EST_REFINE_DEPTH:
+  case RHEA_AMR_INIT_REFINE_DEPTH:
     {
       rhea_amr_p4est_refine_depth_data_t *data = refine_data;
 
@@ -254,4 +255,177 @@ rhea_amr_p4est_refine (p4est_t *p4est,
 
   /* return whether refinement was performed */
   return (refine_fn != NULL);
+}
+
+/******************************************************************************
+ * AMR for ymir
+ *****************************************************************************/
+
+/* types of flags for AMR */
+typedef enum
+{
+  RHEA_AMR_MARKER_COARSEN   = -1,
+  RHEA_AMR_MARKER_NO_CHANGE =  0,
+  RHEA_AMR_MARKER_REFINE    =  1
+}
+rhea_amr_marker_value_t;
+
+static int
+rhea_amr_coarsen_via_marker_fn (p4est_t *p4est, p4est_topidx_t tree,
+                                p4est_quadrant_t *quadrants[])
+{
+  int                 k;
+
+  SC_CHECK_ABORT (p4est_quadrant_is_familypv (quadrants), "Coarsen invocation");
+
+  for (k = 0; k < P4EST_CHILDREN; k++) {
+    rhea_p4est_quadrant_data_t *d = quadrants[k]->p.user_data;
+
+    /* if at least one child is not marked for coarsening */
+    if (d->amr_flag != RHEA_AMR_MARKER_COARSEN) {
+      return 0;
+    }
+  }
+
+  /* if all of the children are marked for coarsening */
+  return 1;
+}
+
+static int
+rhea_amr_refine_via_marker_fn (p4est_t *p4est, p4est_topidx_t tree,
+                               p4est_quadrant_t *quadrant)
+{
+  rhea_p4est_quadrant_data_t *d = quadrant->p.user_data;
+
+  /* if this quadrant is marked for refinement */
+  return (d->amr_flag == RHEA_AMR_MARKER_REFINE);
+}
+
+int
+rhea_amr (p4est_t *p4est,
+          const double n_marked_elements_tol,
+          const int amr_recursive_count,
+          const double n_marked_elements_recursive_tol,
+          rhea_amr_mark_elements_fn_t mark_elements_fn,
+          void *mark_elements_data,
+          rhea_amr_data_initialize_fn_t data_initialize_fn,
+          rhea_amr_data_finalize_fn_t data_finalize_fn,
+          rhea_amr_data_project_fn_t data_project_fn,
+          rhea_amr_data_partition_fn_t data_partition_fn,
+          void *data)
+{
+  const char         *this_fn_name = "rhea_amr";
+  /* arguments for coarsening/refinement */
+  const int           coarsen_recursively = 0;
+  const int           refine_recursively = 0;
+  p4est_coarsen_t     coarsen_fn = rhea_amr_coarsen_via_marker_fn;
+  p4est_refine_t      refine_fn = rhea_amr_refine_via_marker_fn;
+  p4est_init_t        init_fn = rhea_p4est_init_fn;
+  /* arguments for partitioning */
+  const int           partition_for_coarsening = 1;
+  p4est_weight_t      partition_weight_fn = RHEA_AMR_P4EST_PARTITION_WEIGHT_FN;
+
+  double              n_marked_rel;
+  int                 iter;
+
+  RHEA_GLOBAL_INFOF ("Into %s (#elements marked tol. %g, recursive %i, "
+                     "#elements marked recursive tol. %g)\n",
+                     this_fn_name, n_marked_elements_tol, amr_recursive_count,
+                     n_marked_elements_recursive_tol);
+
+  /* check input */
+  RHEA_ASSERT (mark_elements_fn != NULL);
+
+  for (iter = 0; iter < amr_recursive_count; iter++) { /* BEGIN: AMR iter */
+    /*
+     * Mark Elements
+     */
+
+    /* call function that marks elements for coarsening/refinement */
+    n_marked_rel = mark_elements_fn (p4est, mark_elements_data);
+
+    /* print statistics */
+    //TODO
+
+    /* stop AMR if not enough elements were marked */
+    if (0 == iter) {
+      if (isfinite (n_marked_elements_tol) &&
+          n_marked_rel < n_marked_elements_tol) {
+        RHEA_GLOBAL_INFOF (
+            "%s -- iter %i: Exit AMR, rel. #elements marked %g, tolerance %g\n",
+            this_fn_name, iter, n_marked_rel, n_marked_elements_tol);
+        break;
+      }
+    }
+    else {
+      if (isfinite (n_marked_elements_recursive_tol) &&
+          n_marked_rel < n_marked_elements_recursive_tol) {
+        RHEA_GLOBAL_INFOF (
+            "%s -- iter %i: Stop AMR recursion, "
+            "rel. #elements marked %g, tolerance %g\n",
+            this_fn_name, iter, n_marked_rel, n_marked_elements_recursive_tol);
+        break;
+      }
+    }
+
+    /*
+     * Coarsen & Refine Elements
+     */
+
+    /* initialize data at first iteration */
+    if (0 == iter && data_initialize_fn != NULL) {
+      data_initialize_fn (p4est, data);
+    }
+
+    /* coarsen p4est */
+    p4est_coarsen (p4est, coarsen_recursively, coarsen_fn, init_fn);
+
+    /* print statistics */
+    //TODO
+
+    /* refine p4est */
+    p4est_refine (p4est, refine_recursively, refine_fn, init_fn);
+
+    /* print statistics */
+    //TODO
+
+    /* balance p4est */
+    p4est_balance (p4est, P4EST_CONNECT_FULL, init_fn);
+
+    /* print statistics */
+    //TODO
+
+    /* project data onto adapted mesh */
+    if (data_project_fn != NULL) {
+      data_project_fn (p4est, data);
+    }
+
+    /*
+     * Partition
+     */
+
+    /* partition p4est */
+    p4est_partition_ext (p4est, partition_for_coarsening, partition_weight_fn);
+
+    /* print statistics */
+    //TODO
+
+    /* partition data */
+    if (data_partition_fn != NULL) {
+      data_partition_fn (p4est, data);
+    }
+  } /* END: AMR iter */
+
+  /* finalize data if AMR was performed */
+  if (0 < iter && data_finalize_fn != NULL) {
+    data_finalize_fn (p4est, data);
+  }
+
+  /* print statistics */
+  //TODO
+
+  RHEA_GLOBAL_INFOF ("Done %s\n", this_fn_name);
+
+  /* return number of performed AMR iterations */
+  return iter;
 }
--- src/rhea_amr.h
+++ src/rhea_amr.h
@@ -9,12 +9,68 @@
 void                rhea_amr_add_options (ymir_options_t * opt_sup);
 
 /**
- * Performs initial refinement for a p4est mesh.
+ * Performs initial refinement of a p4est mesh.
  */
-int                 rhea_amr_p4est_refine (p4est_t *p4est,
-                                           const int level_min,
-                                           const int level_max,
-                                           rhea_domain_options_t
-                                             *domain_options);
+int                 rhea_amr_init_refine (p4est_t *p4est,
+                                          const int level_min,
+                                          const int level_max,
+                                          rhea_domain_options_t
+                                            *domain_options);
+
+/**
+ * Marks elements of a p4est mesh for coarsening/refinement.
+ *
+ * \param [in] p4est    p4est mesh
+ * \param [in/out] data User data
+ * \return              Relative #elements marked for coarsening/refinement
+ */
+typedef double    (*rhea_amr_mark_elements_fn_t) (p4est_t *p4est, void *data);
+
+/**
+ * Initializes data before mesh is altered through coarsening/refinement.
+ *
+ * \param [in] p4est    p4est mesh before AMR
+ * \param [in/out] data User data
+ */
+typedef void      (*rhea_amr_data_initialize_fn_t) (p4est_t *p4est, void *data);
+
+/**
+ * Finalizes data after AMR is finished.
+ *
+ * \param [in] p4est    p4est mesh after AMR
+ * \param [in/out] data User data
+ */
+typedef void      (*rhea_amr_data_finalize_fn_t) (p4est_t *p4est, void *data);
+
+/**
+ * Projects data to an adapted (i.e., coarsened/refined) mesh.
+ *
+ * \param [in] p4est    Adapted p4est mesh
+ * \param [in/out] data User data
+ */
+typedef void      (*rhea_amr_data_project_fn_t) (p4est_t *p4est, void *data);
+
+/**
+ * Partitions data from one mesh to another.
+ *
+ * \param [in] p4est    Adapted & partitioned p4est mesh
+ * \param [in/out] data User data
+ */
+typedef void      (*rhea_amr_data_partition_fn_t) (p4est_t *p4est, void *data);
+
+/**
+ * Performs adaptive coarsening and refinement of a p4est mesh.
+ */
+int                 rhea_amr (p4est_t *p4est,
+                              const double n_marked_elements_tol,
+                              const int amr_recursive_count,
+                              const double n_marked_elements_recursive_tol,
+                              rhea_amr_mark_elements_fn_t mark_elements_fn,
+                              void *mark_elements_data,
+                              rhea_amr_data_initialize_fn_t data_initialize_fn,
+                              rhea_amr_data_finalize_fn_t data_finalize_fn,
+                              rhea_amr_data_project_fn_t data_project_fn,
+                              rhea_amr_data_partition_fn_t data_partition_fn,
+                              void *data);
 
 #endif /* RHEA_AMR_H */
