a43e19285c9986ecc71bcace24866460f815f602
Johann Rudi <johann@ices.utexas.edu>
2017-11-23 11:30:04 -0600

    rhea_pointcloud_adaptor: Add point cloud specifically for weak zones.

--- src/rhea_pointcloud_adaptor.cxx
+++ src/rhea_pointcloud_adaptor.cxx
@@ -3,27 +3,72 @@
 
 #include <rhea_pointcloud_adaptor.h>
 #include <rhea_pointcloud.hpp>
+#include <rhea_base.h>
 
 SC_EXTERN_C_BEGIN;
+
+/******************************************************************************
+ * Weak Zone Point Cloud
+ *****************************************************************************/
+
+/* weak zone point cloud */
+struct rhea_pointcloud_weakzone
+{
+  rhea_pointcloud_Cloud  *cloud;
+  rhea_pointcloud_KDTree *tree;
+};
+
+rhea_pointcloud_weakzone_t *
+rhea_pointcloud_weakzone_new (const double *point_coordinates,
+                              const int *point_labels,
+                              const size_t n_points)
+{
+  rhea_pointcloud_weakzone_t *ptcl_weak;
+
+  /* create weak zone object */
+  ptcl_weak = RHEA_ALLOC (rhea_pointcloud_weakzone_t, 1);
+
+  /* create point cloud and fill with points */
+  ptcl_weak->cloud = new rhea_pointcloud_Cloud ();
+  ptcl_weak->cloud->set_points (point_coordinates, n_points);
+
+  /* create kd-tree corresponding to point cloud */
+  ptcl_weak->tree = new rhea_pointcloud_KDTree (*(ptcl_weak->cloud));
+
+  return ptcl_weak;
+}
+
+void
+rhea_pointcloud_weakzone_destroy (rhea_pointcloud_weakzone_t *ptcl_weak)
+{
+  delete ptcl_weak->tree;
+  delete ptcl_weak->cloud;
+  RHEA_FREE (ptcl_weak);
+}
+
+
+
+
+//TODO deprecated below:
 
 /******************************************************************************
  * Point Cloud
  *****************************************************************************/
 
-rhea_pointcloud_Cloud *
+rhea_pointcloud_Cloud_t *
 rhea_pointcloud_Cloud_new ()
 {
   return new rhea_pointcloud_Cloud ();
 }
 
 void
-rhea_pointcloud_Cloud_destroy (rhea_pointcloud_Cloud *cloud)
+rhea_pointcloud_Cloud_destroy (rhea_pointcloud_Cloud_t *cloud)
 {
   delete cloud;
 }
 
 void
-rhea_pointcloud_Cloud_set_points (rhea_pointcloud_Cloud *cloud,
+rhea_pointcloud_Cloud_set_points (rhea_pointcloud_Cloud_t *cloud,
                                   const double *point, const size_t n_points)
 {
   cloud->set_points (point, n_points);
@@ -33,29 +78,29 @@ rhea_pointcloud_Cloud_set_points (rhea_pointcloud_Cloud *cloud,
  * KD-Tree
  *****************************************************************************/
 
-rhea_pointcloud_KDTree *
-rhea_pointcloud_KDTree_new (const rhea_pointcloud_Cloud *cloud)
+rhea_pointcloud_KDTree_t *
+rhea_pointcloud_KDTree_new (const rhea_pointcloud_Cloud_t *cloud)
 {
   return new rhea_pointcloud_KDTree (*cloud);
 }
 
 void
-rhea_pointcloud_KDTree_destroy (rhea_pointcloud_KDTree *tree)
+rhea_pointcloud_KDTree_destroy (rhea_pointcloud_KDTree_t *tree)
 {
   delete tree;
 }
 
 double
 rhea_pointcloud_KDTree_find_shortest_distance_single (
-                                          rhea_pointcloud_KDTree *tree,
+                                          rhea_pointcloud_KDTree_t *tree,
                                           const double *target_pt)
 {
-  return tree->find_shortest_distance_single (target_pt);
+  return tree->find_shortest_distance (target_pt);
 }
 
 void
 rhea_pointcloud_KDTree_find_shortest_distance_multi (
-                                          rhea_pointcloud_KDTree *tree,
+                                          rhea_pointcloud_KDTree_t *tree,
                                           double *dist,
                                           const double *target_x,
                                           const double *target_y,
--- src/rhea_pointcloud_adaptor.h
+++ src/rhea_pointcloud_adaptor.h
@@ -10,18 +10,55 @@
 SC_EXTERN_C_BEGIN;
 
 /******************************************************************************
+ * Weak Zone Point Cloud
+ *****************************************************************************/
+
+/* weak zone point cloud (opaque) */
+typedef struct rhea_pointcloud_weakzone rhea_pointcloud_weakzone_t;
+
+rhea_pointcloud_weakzone_t *rhea_pointcloud_weakzone_new (
+                                              const double *point_coordinates,
+                                              const int *point_labels,
+                                              const size_t n_points);
+
+void                rhea_pointcloud_weakzone_destroy (
+                                        rhea_pointcloud_weakzone_t *ptcl_weak);
+
+//TODO rhea_pointcloud_weakzone_set_coordinates
+//TODO rhea_pointcloud_weakzone_set_factors
+//TODO rhea_pointcloud_weakzone_set_labels
+
+/******************************************************************************
+ * Topography Point Cloud
+ *****************************************************************************/
+
+/* topography point cloud (opaque) */
+typedef struct rhea_pointcloud_topography rhea_pointcloud_topography_t;
+
+//TODO rhea_pointcloud_topography_new
+//TODO rhea_pointcloud_topography_destroy
+//TODO rhea_pointcloud_topography_set_coordinates (2D?)
+//TODO rhea_pointcloud_topography_set_displacements
+//TODO rhea_pointcloud_topography_set_labels
+
+
+
+
+//TODO deprecated below:
+
+/******************************************************************************
  * Point Cloud
  *****************************************************************************/
 
-typedef struct rhea_pointcloud_Cloud rhea_pointcloud_Cloud;
+typedef struct rhea_pointcloud_Cloud rhea_pointcloud_Cloud_t;
 
-rhea_pointcloud_Cloud *rhea_pointcloud_Cloud_new ();
+rhea_pointcloud_Cloud_t *rhea_pointcloud_Cloud_new ();
 
 void                rhea_pointcloud_Cloud_destroy (
-                                          rhea_pointcloud_Cloud *cloud);
+                                          rhea_pointcloud_Cloud_t *cloud);
 
 void                rhea_pointcloud_Cloud_set_points (
-                                          rhea_pointcloud_Cloud *cloud,
+                                          rhea_pointcloud_Cloud_t *cloud,
                                           const double *point,
                                           const size_t n_points);
 
@@ -29,20 +66,20 @@ void                rhea_pointcloud_Cloud_set_points (
  * KD-Tree
  *****************************************************************************/
 
-typedef struct rhea_pointcloud_KDTree rhea_pointcloud_KDTree;
+typedef struct rhea_pointcloud_KDTree rhea_pointcloud_KDTree_t;
 
-rhea_pointcloud_KDTree *rhea_pointcloud_KDTree_new (
-                                          const rhea_pointcloud_Cloud *cloud);
+rhea_pointcloud_KDTree_t *rhea_pointcloud_KDTree_new (
+                                          const rhea_pointcloud_Cloud_t *cloud);
 
 void                rhea_pointcloud_KDTree_destroy (
-                                          rhea_pointcloud_KDTree *tree);
+                                          rhea_pointcloud_KDTree_t *tree);
 
 double              rhea_pointcloud_KDTree_find_shortest_distance_single (
-                                          rhea_pointcloud_KDTree *tree,
+                                          rhea_pointcloud_KDTree_t *tree,
                                           const double *target_pt);
 
 void                rhea_pointcloud_KDTree_find_shortest_distance_multi (
-                                          rhea_pointcloud_KDTree *tree,
+                                          rhea_pointcloud_KDTree_t *tree,
                                           double *dist,
                                           const double *target_x,
                                           const double *target_y,
