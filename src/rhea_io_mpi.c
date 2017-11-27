7d5a418a8b96498182c6d8237a39545e86ba9ef5
Johann Rudi <johann@ices.utexas.edu>
2017-11-26 23:00:58 -0600

    rhea_io_mpi: Add function to read & scatter an array of double values.

--- src/rhea_io_mpi.c
+++ src/rhea_io_mpi.c
@@ -6,12 +6,13 @@
 #include <rhea_base.h>
 
 void
-rhea_io_mpi_read_bcast_double (double *values, const size_t n_entries,
-                               const char *file_path_bin,
-                               const char *file_path_txt,
-                               sc_MPI_Comm mpicomm)
+rhea_io_mpi_read_broadcast_double (double *values_all,
+                                   const int n_entries,
+                                   const char *file_path_bin,
+                                   const char *file_path_txt,
+                                   sc_MPI_Comm mpicomm)
 {
-  const char         *this_fn_name = "rhea_io_mpi_read_bcast_double";
+  const char         *this_fn_name = "rhea_io_mpi_read_broadcast_double";
   int                 mpirank, mpiret;
 
   if (file_path_txt == NULL) { /* if read from binary file */
@@ -28,22 +29,99 @@ rhea_io_mpi_read_bcast_double (double *values, const size_t n_entries,
   /* get parallel environment */
   mpiret = sc_MPI_Comm_rank (mpicomm, &mpirank); SC_CHECK_MPI (mpiret);
 
-  /* read from file with one processor */
+  /* read file with one processor */
   if (mpirank == 0) {
+    const size_t        total_size = (size_t) n_entries;
+
     if (file_path_txt == NULL) { /* if read from binary file */
-      rhea_io_std_read_double (values, n_entries, file_path_bin);
+      rhea_io_std_read_double (values_all, total_size, file_path_bin);
     }
     else { /* otherwise read from text file */
-      rhea_io_std_read_double_from_txt (values, n_entries, file_path_txt);
+      rhea_io_std_read_double_from_txt (values_all, total_size, file_path_txt);
       if (file_path_bin != NULL) {
-        rhea_io_std_write_double (file_path_bin, values, n_entries);
+        rhea_io_std_write_double (file_path_bin, values_all, total_size);
       }
     }
   }
 
   /* broadcast values to all processors */
-  mpiret = sc_MPI_Bcast (values, (int) n_entries, sc_MPI_DOUBLE, 0, mpicomm);
+  mpiret = sc_MPI_Bcast (values_all, n_entries, sc_MPI_DOUBLE, 0, mpicomm);
   SC_CHECK_MPI (mpiret);
+
+  if (file_path_txt == NULL) { /* if read from binary file */
+    RHEA_GLOBAL_INFOF ("Done %s (%s)\n", this_fn_name, file_path_bin);
+  }
+  else {
+    RHEA_GLOBAL_INFOF ("Done %s (bin: %s, txt: %s)\n", this_fn_name,
+                       file_path_bin, file_path_txt);
+  }
+}
+
+void
+rhea_io_mpi_read_scatter_double (double *values_segment,
+                                 const int *segment_offset,
+                                 const char *file_path_bin,
+                                 const char *file_path_txt,
+                                 sc_MPI_Comm mpicomm)
+{
+  const char         *this_fn_name = "rhea_io_mpi_read_scatter_double";
+  int                 mpisize, mpirank, mpiret;
+  double             *values_all;
+  int                *segment_size;
+  int                 r;
+
+  if (file_path_txt == NULL) { /* if read from binary file */
+    RHEA_GLOBAL_INFOF ("Into %s (%s)\n", this_fn_name, file_path_bin);
+  }
+  else {
+    RHEA_GLOBAL_INFOF ("Into %s (bin: %s, txt: %s)\n", this_fn_name,
+                       file_path_bin, file_path_txt);
+  }
+
+  /* check input */
+  RHEA_ASSERT (file_path_bin != NULL || file_path_txt != NULL);
+
+  /* get parallel environment */
+  mpiret = sc_MPI_Comm_size (mpicomm, &mpisize); SC_CHECK_MPI (mpiret);
+  mpiret = sc_MPI_Comm_rank (mpicomm, &mpirank); SC_CHECK_MPI (mpiret);
+
+  /* read file with one processor */
+  if (mpirank == 0) {
+    const size_t        total_size = (size_t) segment_offset[mpisize];
+
+    values_all = RHEA_ALLOC (double, total_size);
+    if (file_path_txt == NULL) { /* if read from binary file */
+      rhea_io_std_read_double (values_all, total_size, file_path_bin);
+    }
+    else { /* otherwise read from text file */
+      rhea_io_std_read_double_from_txt (values_all, total_size, file_path_txt);
+      if (file_path_bin != NULL) {
+        rhea_io_std_write_double (file_path_bin, values_all, total_size);
+      }
+    }
+  }
+  else {
+    values_all = NULL;
+  }
+
+  /* create segment size array */
+  segment_size = RHEA_ALLOC (int, mpisize);
+  for (r = 0; r < mpisize; r++) {
+    segment_size[r] = segment_offset[r+1] - segment_offset[r];
+  }
+
+  /* scatter segments to corresponding processors */
+  mpiret = MPI_Scatterv (
+      values_all, segment_size, segment_offset, sc_MPI_DOUBLE,
+      values_segment, segment_size[mpirank], sc_MPI_DOUBLE, 0, mpicomm);
+  SC_CHECK_MPI (mpiret);
+  //TODO `sc_MPI_Scatterv` is not defined
+
+  /* destroy */
+  if (mpirank == 0) {
+    RHEA_FREE (values_all);
+  }
+  RHEA_FREE (segment_size);
 
   if (file_path_txt == NULL) { /* if read from binary file */
     RHEA_GLOBAL_INFOF ("Done %s (%s)\n", this_fn_name, file_path_bin);
--- src/rhea_io_mpi.h
+++ src/rhea_io_mpi.h
@@ -7,13 +7,24 @@
 #include <sc.h>
 
 /**
- * Reads double values from a binary file with one processor (MPI rank), then
- * broadcasts to all processors.
+ * Reads double values from a binary or text file with one processor (i.e., MPI
+ * rank), then broadcasts to all processors.
  */
-void                rhea_io_mpi_read_bcast_double (double *values,
-                                                   const size_t n_entries,
-                                                   const char *file_path_bin,
-                                                   const char *file_path_txt,
-                                                   sc_MPI_Comm mpicomm);
+void                rhea_io_mpi_read_broadcast_double (
+                                                    double *values_all,
+                                                    const int n_entries,
+                                                    const char *file_path_bin,
+                                                    const char *file_path_txt,
+                                                    sc_MPI_Comm mpicomm);
+
+/**
+ * Reads double values from a binary or text file with one processor (i.e., MPI
+ * rank), then scatters corresponding segments to all processors.
+ */
+void                rhea_io_mpi_read_scatter_double (double *values_segment,
+                                                     const int *segment_offset,
+                                                     const char *file_path_bin,
+                                                     const char *file_path_txt,
+                                                     sc_MPI_Comm mpicomm);
 
 #endif /* RHEA_IO_MPI_H */
