0bfc7ebe9ee7cabb0b7b798746159c69a3ccbe4d
Johann Rudi <johann@ices.utexas.edu>
2017-12-01 16:35:05 -0600

    Example share: Update interface to Stokes problem.

--- example/share/example_share_stokes.c
+++ example/share/example_share_stokes.c
@@ -24,7 +24,6 @@ example_share_stokes_new (rhea_stokes_problem_t **stokes_problem,
   rhea_domain_options_t *domain_options = visc_options->domain_options;
   sc_MPI_Comm         mpicomm = ymir_mesh_get_MPI_Comm (ymir_mesh);
   ymir_vec_t         *temperature;
-  ymir_vec_t         *rhs_vel, *rhs_vel_nonzero_dirichlet;
   void               *solver_options;
 
   RHEA_GLOBAL_PRODUCTIONF ("Into %s\n", this_fn_name);
@@ -35,13 +34,6 @@ example_share_stokes_new (rhea_stokes_problem_t **stokes_problem,
   /* compute temperature */
   temperature = rhea_temperature_new (ymir_mesh);
   rhea_temperature_compute (temperature, temp_options);
-
-  /* create velocity right-hand side volume forcing */
-  rhs_vel = rhea_velocity_new (ymir_mesh);
-  rhea_temperature_compute_rhs_vel (rhs_vel, temperature, temp_options);
-
-  /* create the nonzero velocity values at the Dirichlet boundary */
-  rhs_vel_nonzero_dirichlet = NULL; //TODO
 
   /* set solver-specific variables */
   switch (visc_options->type) {
@@ -57,8 +49,8 @@ example_share_stokes_new (rhea_stokes_problem_t **stokes_problem,
 
   /* create Stokes problem */
   *stokes_problem = rhea_stokes_problem_new (
-      temperature, rhs_vel, rhs_vel_nonzero_dirichlet, ymir_mesh, press_elem,
-      domain_options, weak_options, visc_options, solver_options);
+      ymir_mesh, press_elem, temperature, domain_options, temp_options,
+      weak_options, visc_options, solver_options);
   if (RHEA_VISCOSITY_NONLINEAR == visc_options->type) {
     rhea_stokes_problem_nonlinear_set_output (vtk_write_newton_itn_path,
                                               *stokes_problem);
@@ -75,15 +67,11 @@ example_share_stokes_destroy (rhea_stokes_problem_t *stokes_problem,
 {
   const char         *this_fn_name = "example_share_stokes_destroy";
   ymir_vec_t         *temperature;
-  ymir_vec_t         *rhs_vel, *rhs_vel_nonzero_dirichlet;
 
   RHEA_GLOBAL_PRODUCTIONF ("Into %s\n", this_fn_name);
 
-  /* get vectors */
+  /* get temperature */
   temperature = rhea_stokes_problem_get_temperature (stokes_problem);
-  rhs_vel = rhea_stokes_problem_get_rhs_vel (stokes_problem);
-  rhs_vel_nonzero_dirichlet =
-    rhea_stokes_problem_get_rhs_vel_nonzero_dirichlet (stokes_problem);
 
   /* destroy Stokes problem */
   rhea_stokes_problem_destroy (stokes_problem);
@@ -91,12 +79,6 @@ example_share_stokes_destroy (rhea_stokes_problem_t *stokes_problem,
   /* destroy vectors */
   if (temperature != NULL) {
     rhea_temperature_destroy (temperature);
-  }
-  if (rhs_vel != NULL) {
-    rhea_velocity_destroy (rhs_vel);
-  }
-  if (rhs_vel_nonzero_dirichlet != NULL) {
-    rhea_velocity_destroy (rhs_vel_nonzero_dirichlet);
   }
 
   /* destroy data */
