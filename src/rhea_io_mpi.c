3f2817b4a099050484a8565b4301c1a0f40bc4cb
Johann Rudi <johann@ices.utexas.edu>
2017-12-04 15:10:32 -0600

    rhea_io_mpi: Add function to gather/write double values.

--- src/rhea_io_mpi.c
+++ src/rhea_io_mpi.c
@@ -202,3 +202,61 @@ rhea_io_mpi_read_scatter_double (double *values_segment,
                        file_path_bin, file_path_txt);
   }
 }
+
+void
+rhea_io_mpi_gather_write_double_to_txt (const char *file_path_txt,
+                                        const double *values_segment,
+                                        const int *segment_offset,
+                                        int n_entries_per_line,
+                                        sc_MPI_Comm mpicomm)
+{
+  const char         *this_fn_name = "rhea_io_mpi_gather_write_double_to_txt";
+  int                 mpisize, mpirank, mpiret;
+  double             *values_all;
+  size_t              total_size;
+  int                *segment_size;
+  int                 r;
+
+  RHEA_GLOBAL_INFOF ("Into %s (%s)\n", this_fn_name, file_path_txt);
+
+  /* get parallel environment */
+  mpiret = sc_MPI_Comm_size (mpicomm, &mpisize); SC_CHECK_MPI (mpiret);
+  mpiret = sc_MPI_Comm_rank (mpicomm, &mpirank); SC_CHECK_MPI (mpiret);
+
+  /* set total (=global) size */
+  RHEA_ASSERT (0 < segment_offset[mpisize]);
+  total_size = (size_t) segment_offset[mpisize];
+
+  /* create segment size array */
+  segment_size = RHEA_ALLOC (int, mpisize);
+  for (r = 0; r < mpisize; r++) {
+    segment_size[r] = segment_offset[r+1] - segment_offset[r];
+  }
+
+  /* gather segments from all processors */
+  if (mpirank == 0) {
+    values_all = RHEA_ALLOC (double, total_size);
+  }
+  else {
+    values_all = NULL;
+  }
+  mpiret = MPI_Gatherv (
+      values_segment, segment_size[mpirank], sc_MPI_DOUBLE,
+      values_all, segment_size, segment_offset, sc_MPI_DOUBLE, 0, mpicomm);
+  SC_CHECK_MPI (mpiret);
+  //TODO `sc_MPI_Gatherv` is not defined
+
+  /* write file with one processor */
+  if (mpirank == 0) {
+    rhea_io_std_write_double_to_txt (file_path_txt, values_all, total_size,
+                                     n_entries_per_line);
+  }
+
+  /* destroy */
+  if (mpirank == 0) {
+    RHEA_FREE (values_all);
+  }
+  RHEA_FREE (segment_size);
+
+  RHEA_GLOBAL_INFOF ("Done %s (%s)\n", this_fn_name, file_path_txt);
+}
--- src/rhea_io_mpi.h
+++ src/rhea_io_mpi.h
@@ -21,8 +21,7 @@ int                 rhea_io_mpi_read_broadcast_double (
  * Reads int values from a binary or text file with one processor (i.e., MPI
  * rank), then broadcasts to all processors.
  */
-int                 rhea_io_mpi_read_broadcast_int (
-                                                    int *values_all,
+int                 rhea_io_mpi_read_broadcast_int (int *values_all,
                                                     int n_entries,
                                                     const char *file_path_bin,
                                                     const char *file_path_txt,
@@ -37,5 +36,16 @@ void                rhea_io_mpi_read_scatter_double (double *values_segment,
                                                      const char *file_path_bin,
                                                      const char *file_path_txt,
                                                      sc_MPI_Comm mpicomm);
+
+/**
+ * Gathers double values from all processors, then writes to a text file with
+ * one processor (i.e., MPI rank).
+ */
+void                rhea_io_mpi_gather_write_double_to_txt (
+                                                  const char *file_path_txt,
+                                                  const double *values_segment,
+                                                  const int *segment_offset,
+                                                  int n_entries_per_line,
+                                                  sc_MPI_Comm mpicomm);
 
 #endif /* RHEA_IO_MPI_H */
