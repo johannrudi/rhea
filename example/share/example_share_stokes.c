b002988aac6dab6ed6c6ceb9c0e7c8dfd6b47f2e
Johann Rudi <johann@ices.utexas.edu>
2017-12-15 15:28:30 -0600

    Example share: Perform initial AMR and activate AMR within solver (e.g., Newton).

--- example/share/example_share_stokes.c
+++ example/share/example_share_stokes.c
@@ -9,20 +9,23 @@
 #include <rhea_base.h>
 #include <rhea_velocity.h>
 #include <rhea_weakzone.h>
+#include <rhea_stokes_problem_amr.h>
 
 void
 example_share_stokes_new (rhea_stokes_problem_t **stokes_problem,
-                          ymir_mesh_t *ymir_mesh,
-                          ymir_pressure_elem_t *press_elem,
+                          ymir_mesh_t **ymir_mesh,
+                          ymir_pressure_elem_t **press_elem,
                           rhea_temperature_options_t *temp_options,
                           rhea_weakzone_options_t *weak_options,
                           rhea_viscosity_options_t *visc_options,
                           rhea_newton_options_t *newton_options,
+                          p4est_t *p4est,
+                          rhea_discretization_options_t *discr_options,
                           char *solver_vtk_path)
 {
   const char         *this_fn_name = "example_share_stokes_new";
   rhea_domain_options_t *domain_options = visc_options->domain_options;
-  sc_MPI_Comm         mpicomm = ymir_mesh_get_MPI_Comm (ymir_mesh);
+  sc_MPI_Comm         mpicomm = ymir_mesh_get_MPI_Comm (*ymir_mesh);
   ymir_vec_t         *temperature;
   void               *solver_options;
 
@@ -32,7 +35,7 @@ example_share_stokes_new (rhea_stokes_problem_t **stokes_problem,
   rhea_weakzone_data_create (weak_options, mpicomm);
 
   /* compute temperature */
-  temperature = rhea_temperature_new (ymir_mesh);
+  temperature = rhea_temperature_new (*ymir_mesh);
   rhea_temperature_compute (temperature, temp_options);
 
   /* set solver-specific variables */
@@ -49,9 +52,19 @@ example_share_stokes_new (rhea_stokes_problem_t **stokes_problem,
 
   /* create Stokes problem */
   *stokes_problem = rhea_stokes_problem_new (
-      ymir_mesh, press_elem, temperature, domain_options, temp_options,
+      *ymir_mesh, *press_elem, temperature, domain_options, temp_options,
       weak_options, visc_options, solver_options);
+  rhea_stokes_problem_set_solver_amr (*stokes_problem, p4est, discr_options);
   rhea_stokes_problem_set_solver_vtk_output (*stokes_problem, solver_vtk_path);
+
+  /* perform initial AMR */
+  if (p4est != NULL && discr_options != NULL) {
+    rhea_stokes_problem_init_amr (*stokes_problem, p4est, discr_options);
+
+    /* retrieve adapted mesh */
+    *ymir_mesh = rhea_stokes_problem_get_ymir_mesh (*stokes_problem);
+    *press_elem = rhea_stokes_problem_get_press_elem (*stokes_problem);
+  }
 
   RHEA_GLOBAL_PRODUCTIONF ("Done %s\n", this_fn_name);
 }
--- example/share/example_share_stokes.h
+++ example/share/example_share_stokes.h
@@ -19,22 +19,24 @@
  * Creates a new Stokes problem.
  */
 void                example_share_stokes_new (
-                                      rhea_stokes_problem_t **stokes_problem,
-                                      ymir_mesh_t *ymir_mesh,
-                                      ymir_pressure_elem_t *press_elem,
-                                      rhea_temperature_options_t *temp_options,
-                                      rhea_weakzone_options_t *weak_options,
-                                      rhea_viscosity_options_t *visc_options,
-                                      rhea_newton_options_t *newton_options,
-                                      char *solver_vtk_path);
+                                  rhea_stokes_problem_t **stokes_problem,
+                                  ymir_mesh_t **ymir_mesh,
+                                  ymir_pressure_elem_t **press_elem,
+                                  rhea_temperature_options_t *temp_options,
+                                  rhea_weakzone_options_t *weak_options,
+                                  rhea_viscosity_options_t *visc_options,
+                                  rhea_newton_options_t *newton_options,
+                                  p4est_t *p4est,
+                                  rhea_discretization_options_t *discr_options,
+                                  char *solver_vtk_path);
 
 /**
  * Destroys a Stokes problem.
  */
 void                example_share_stokes_destroy (
-                                      rhea_stokes_problem_t *stokes_problem,
-                                      rhea_temperature_options_t *temp_options,
-                                      rhea_weakzone_options_t *weak_options,
-                                      rhea_viscosity_options_t *visc_options);
+                                  rhea_stokes_problem_t *stokes_problem,
+                                  rhea_temperature_options_t *temp_options,
+                                  rhea_weakzone_options_t *weak_options,
+                                  rhea_viscosity_options_t *visc_options);
 
 #endif /* EXAMPLE_SHARE_STOKES_H */
