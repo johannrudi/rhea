badc06e111dd06a1379d90ae15db95f98d136512
Johann Rudi <johann@ices.utexas.edu>
2018-05-16 14:44:21 -0500

    Add new files: rhea_plate.c/h

---
+++ src/rhea_plate.c
@@ -0,0 +1,101 @@
+#include <rhea_plate.h>
+#include <rhea_base.h>
+#include <rhea_point_in_polygon.h>
+
+static int
+rhea_plate_is_inside (const float test_x,
+                      const float test_y,
+                      const float *_sc_restrict vert_x,
+                      const float *_sc_restrict vert_y,
+                      const size_t n_vert,
+                      const float *_sc_restrict vert_coarse_container_x,
+                      const float *_sc_restrict vert_coarse_container_y,
+                      const size_t n_vert_coarse_container)
+{
+  /* check input */
+  RHEA_ASSERT (vert_x != NULL);
+  RHEA_ASSERT (vert_y != NULL);
+  RHEA_ASSERT (0 < n_vert);
+  RHEA_ASSERT (0 >= n_vert_coarse_container || vert_coarse_container_x != NULL);
+  RHEA_ASSERT (0 >= n_vert_coarse_container || vert_coarse_container_y != NULL);
+
+  /* check coarse container of plate */
+  if (0 < n_vert_coarse_container) {
+    int                 is_inside;
+
+    /* run point-in-polygon test on coarse container */
+    is_inside = rhea_point_in_polygon_is_inside (
+        test_x, test_y, vert_coarse_container_x, vert_coarse_container_y,
+        n_vert_coarse_container);
+
+    /* exit if point is outside of container */
+    if (!is_inside) {
+      return 0;
+    }
+  }
+
+  /* check fine-resolution boundary of plate */
+  return rhea_point_in_polygon_is_inside (test_x, test_y, vert_x, vert_y,
+                                          n_vert);
+}
+
+static int
+rhea_plate_get_label (const float test_x,
+                      const float test_y,
+                      const float **plate_vert_x,
+                      const float **plate_vert_y,
+                      const size_t *plate_n_vert,
+                      const float **plate_vert_coarse_container_x,
+                      const float **plate_vert_coarse_container_y,
+                      const size_t *plate_n_vert_coarse_container,
+                      const int n_plates)
+{
+  int                 k;
+
+  for (k = 0; k < n_plates; k++) { /* loop over all plates */
+    const float        *_sc_restrict vx = plate_vert_x[k];
+    const float        *_sc_restrict vy = plate_vert_y[k];
+    const size_t        nv = plate_n_vert[k];
+    const float        *_sc_restrict vcx = plate_vert_coarse_container_x[k];
+    const float        *_sc_restrict vcy = plate_vert_coarse_container_y[k];
+    const size_t        nvc = plate_n_vert_coarse_container[k];
+
+    if (rhea_plate_is_inside (test_x, test_y, vx, vy, nv, vcx, vcy, nvc)) {
+      /* return that test coordinates are inside current plate */
+      return k;
+    }
+  }
+
+  /* return that test coordinates are outside of any plate */
+  return RHEA_PLATE_NONE;
+}
+
+//TODO read from options
+const float **plate_vert_x;
+const float **plate_vert_y;
+const size_t *plate_n_vert;
+const float **plate_vert_coarse_container_x;
+const float **plate_vert_coarse_container_y;
+const size_t *plate_n_vert_coarse_container;
+
+rhea_plate_cube_label_t
+rhea_plate_cube_get_label (const double test_x, const double test_y)
+{
+  const int           n_plates = RHEA_PLATE_CUBE_N;
+
+  return (rhea_plate_cube_label_t) rhea_plate_get_label (
+      (float) test_x, (float) test_y, plate_vert_x, plate_vert_y, plate_n_vert,
+      plate_vert_coarse_container_x, plate_vert_coarse_container_y,
+      plate_n_vert_coarse_container, n_plates);
+}
+
+rhea_plate_earth_label_t
+rhea_plate_earth_get_label (const double test_x, const double test_y)
+{
+  const int           n_plates = RHEA_PLATE_EARTH_N;
+
+  return (rhea_plate_earth_label_t) rhea_plate_get_label (
+      (float) test_x, (float) test_y, plate_vert_x, plate_vert_y, plate_n_vert,
+      plate_vert_coarse_container_x, plate_vert_coarse_container_y,
+      plate_n_vert_coarse_container, n_plates);
+}
---
+++ src/rhea_plate.h
@@ -0,0 +1,110 @@
+/* RHEA_PLATE TODO write comment */
+
+#ifndef RHEA_PLATE_H
+#define RHEA_PLATE_H
+
+#include <sc.h>
+
+/* generic label for plate "none" */
+#define RHEA_PLATE_NONE (-1)
+
+/******************************************************************************
+ * Options
+ *****************************************************************************/
+
+//TODO
+
+/******************************************************************************
+ * Plates of Cube Domain
+ *****************************************************************************/
+
+typedef enum
+{
+  RHEA_PLATE_CUBE_NONE = RHEA_PLATE_NONE, /* "none" (must come first) */
+  RHEA_PLATE_CUBE_NW = 0,                 /* North-West (must start at zero) */
+  RHEA_PLATE_CUBE_NE,                     /* North-East */
+  RHEA_PLATE_CUBE_SE,                     /* South-East */
+  RHEA_PLATE_CUBE_SW,                     /* South-West */
+  RHEA_PLATE_CUBE_N                       /* (number of plates) */
+}
+rhea_plate_cube_label_t;
+
+rhea_plate_cube_label_t   rhea_plate_cube_get_label (const double test_x,
+                                                     const double test_y);
+
+/******************************************************************************
+ * Plates of Earth Domain
+ *****************************************************************************/
+
+typedef enum
+{
+  RHEA_PLATE_EARTH_NONE = RHEA_PLATE_NONE,  /* "none" (must come first) */
+
+  /* MORVEL (25 plates) */
+  RHEA_PLATE_EARTH_AM = 0,  /* Amur (must start at zero) */
+  RHEA_PLATE_EARTH_AN,      /* Antarctic */
+  RHEA_PLATE_EARTH_AR,      /* Arabia */
+  RHEA_PLATE_EARTH_AU,      /* Australia */
+  RHEA_PLATE_EARTH_CP,      /* Capricorn */
+  RHEA_PLATE_EARTH_CA,      /* Caribbean */
+  RHEA_PLATE_EARTH_CO,      /* Cocos */
+  RHEA_PLATE_EARTH_EU,      /* Eurasia */
+  RHEA_PLATE_EARTH_IN,      /* India */
+  RHEA_PLATE_EARTH_JF,      /* Juan de Fuca */
+  RHEA_PLATE_EARTH_LW,      /* Lwandle */
+  RHEA_PLATE_EARTH_MQ,      /* Macquarie */
+  RHEA_PLATE_EARTH_NZ,      /* Nazca */
+  RHEA_PLATE_EARTH_NA,      /* North America */
+  RHEA_PLATE_EARTH_NU,      /* Nubia */
+  RHEA_PLATE_EARTH_PA,      /* Pacific */
+  RHEA_PLATE_EARTH_PS,      /* Philippine Sea */
+  RHEA_PLATE_EARTH_RI,      /* Rivera */
+  RHEA_PLATE_EARTH_SW,      /* Sandwich */
+  RHEA_PLATE_EARTH_SC,      /* Scotia */
+  RHEA_PLATE_EARTH_SM,      /* Somalia */
+  RHEA_PLATE_EARTH_SA,      /* South America */
+  RHEA_PLATE_EARTH_SU,      /* Sundaland */
+  RHEA_PLATE_EARTH_SR,      /* Sur */
+  RHEA_PLATE_EARTH_YZ,      /* Yangtze */
+
+  /* Bird, 2003 (31 plates) */
+  RHEA_PLATE_EARTH_AS,      /* Aegean Sea */
+  RHEA_PLATE_EARTH_AP,      /* Altiplano */
+  RHEA_PLATE_EARTH_AT,      /* Anatolia */
+  RHEA_PLATE_EARTH_BR,      /* Bahnoral Reef */
+  RHEA_PLATE_EARTH_BS,      /* Banda Sea */
+  RHEA_PLATE_EARTH_BH,      /* Birds Head */
+  RHEA_PLATE_EARTH_BU,      /* Burma */
+  RHEA_PLATE_EARTH_CL,      /* Caroline */
+  RHEA_PLATE_EARTH_CR,      /* Conway Reef */
+  RHEA_PLATE_EARTH_EA,      /* Easter */
+  RHEA_PLATE_EARTH_FT,      /* Futuna */
+  RHEA_PLATE_EARTH_GP,      /* Galapagos */
+  RHEA_PLATE_EARTH_JZ,      /* Juan Fernandez */
+  RHEA_PLATE_EARTH_KE,      /* Kermadec */
+  RHEA_PLATE_EARTH_MN,      /* Manus */
+  RHEA_PLATE_EARTH_MO,      /* Maoke */
+  RHEA_PLATE_EARTH_MA,      /* Mariana */
+  RHEA_PLATE_EARTH_MS,      /* Molucca Sea */
+  RHEA_PLATE_EARTH_NH,      /* New Hebrides */
+  RHEA_PLATE_EARTH_NI,      /* Niuafo'ou */
+  RHEA_PLATE_EARTH_ND,      /* North Andes */
+  RHEA_PLATE_EARTH_NB,      /* North Bismarck */
+  RHEA_PLATE_EARTH_OK,      /* Okhotsk */
+  RHEA_PLATE_EARTH_ON,      /* Okinawa */
+  RHEA_PLATE_EARTH_PM,      /* Panama */
+  RHEA_PLATE_EARTH_SL,      /* Shetland */
+  RHEA_PLATE_EARTH_SS,      /* Solomon Sea */
+  RHEA_PLATE_EARTH_SB,      /* South Bismarck */
+  RHEA_PLATE_EARTH_TI,      /* Timor */
+  RHEA_PLATE_EARTH_TO,      /* Tonga */
+  RHEA_PLATE_EARTH_WL,      /* Woodlark */
+
+  RHEA_PLATE_EARTH_N        /* (number of plates) */
+}
+rhea_plate_earth_label_t;
+
+rhea_plate_earth_label_t  rhea_plate_earth_get_label (const double test_x,
+                                                      const double test_y);
+
+#endif /* RHEA_PLATE_H */
