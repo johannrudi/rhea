1e18a7348190a623eadbc1c824bae22ffa2ccd83
Johann Rudi <johann@ices.utexas.edu>
2018-06-11 22:33:09 -0500

    rhea_base: Add more macros for beginning/end of funcitons.

--- src/rhea_base.h
+++ src/rhea_base.h
@@ -110,10 +110,12 @@ void                RHEA_GLOBAL_LERRORF (const char *fmt, ...)
 #define RHEA_GLOBAL_NOTICEF       RHEA_GLOBAL_STATISTICSF
 
 /* global log macros for beginning and end of a function (XML type output) */
-#define RHEA_GLOBAL_VERBOSE_FN_BEGIN(fn)   RHEA_GLOBAL_VERBOSEF ("<%s>\n", fn)
-#define RHEA_GLOBAL_VERBOSE_FN_END(fn)     RHEA_GLOBAL_VERBOSEF ("</%s>\n", fn)
-#define RHEA_GLOBAL_INFO_FN_BEGIN(fn)      RHEA_GLOBAL_INFOF ("<%s>\n", fn)
-#define RHEA_GLOBAL_INFO_FN_END(fn)        RHEA_GLOBAL_INFOF ("</%s>\n", fn)
+#define RHEA_GLOBAL_VERBOSE_FN_BEGIN(fn)    RHEA_GLOBAL_VERBOSEF ("<%s>\n", fn)
+#define RHEA_GLOBAL_VERBOSE_FN_END(fn)      RHEA_GLOBAL_VERBOSEF ("</%s>\n", fn)
+#define RHEA_GLOBAL_INFO_FN_BEGIN(fn)       RHEA_GLOBAL_INFOF ("<%s>\n", fn)
+#define RHEA_GLOBAL_INFO_FN_END(fn)         RHEA_GLOBAL_INFOF ("</%s>\n", fn)
+#define RHEA_GLOBAL_PRODUCTION_FN_BEGIN(fn) RHEA_GLOBAL_INFOF ("<%s>\n", fn)
+#define RHEA_GLOBAL_PRODUCTION_FN_END(fn)   RHEA_GLOBAL_INFOF ("</%s>\n", fn)
 
 #define RHEA_GLOBAL_VERBOSEF_FN_BEGIN(fn,a,...) \
         RHEA_GLOBAL_VERBOSEF ("<%s "a">\n", fn, __VA_ARGS__)
@@ -123,6 +125,10 @@ void                RHEA_GLOBAL_LERRORF (const char *fmt, ...)
         RHEA_GLOBAL_INFOF ("<%s "a">\n", fn, __VA_ARGS__)
 #define RHEA_GLOBAL_INFOF_FN_END(fn,a,...) \
         RHEA_GLOBAL_INFOF ("</%s "a">\n", fn, __VA_ARGS__)
+#define RHEA_GLOBAL_PRODUCTIONF_FN_BEGIN(fn,a,...) \
+        RHEA_GLOBAL_PRODUCTIONF ("<%s "a">\n", fn, __VA_ARGS__)
+#define RHEA_GLOBAL_PRODUCTIONF_FN_END(fn,a,...) \
+        RHEA_GLOBAL_PRODUCTIONF ("</%s "a">\n", fn, __VA_ARGS__)
 
 /* log macros that are active on every processor */
 #define RHEA_TRACE(s)      RHEA_LOG (SC_LP_TRACE, (s))
