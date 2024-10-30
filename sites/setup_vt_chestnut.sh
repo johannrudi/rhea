8a99030f185a0ac2e1743e225be2b5d8b2faf9fa
Johann Rudi <jrudi@vt.edu>
2024-10-30 16:30:34 +0000

    sites: Update configure script for Chestnut.

--- sites/setup_vt_chestnut.sh
+++ sites/setup_vt_chestnut.sh
@@ -2,6 +2,7 @@
 module purge
 
 # load modules
-module load icc/2023.1.0 
-module load mpi/2021.9.0 
-module load mkl/2023.1.0
+module load mkl/latest
+
+# load virtual environment
+#source /home/jrudi/venv/rhea-kit/bin/activate
